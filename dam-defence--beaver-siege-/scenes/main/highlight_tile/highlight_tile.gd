11900779206a520006db6a414d62cdf713c28b76
Crypt-cat <tomibrew806@gmail.com>
2025-06-16 23:05:52 +1200

    Placeable towers and highlight tiles

---
+++ dam-defence--beaver-siege-/scenes/main/highlight_tile/highlight_tile.gd
@@ -0,0 +1,12 @@
+class_name HighlightTile
+extends Node2D
+
+
+func _process(delta: float) -> void:
+	follow_mouse_position()
+
+
+func follow_mouse_position() -> void:
+	var mouse_position : Vector2i = get_global_mouse_position() / 16
+	
+	position = mouse_position * 16
